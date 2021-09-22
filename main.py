7a58133bc23ac28052c242ae3ee9ebbd9b56907a
Michał Kruszewski <mkru@protonmail.com>
2021-09-22 16:49:41 +0200

    Add some command line arguments.

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
 #!/bin/python3
 
+import argparse
+
 VERSION = "0.0.0"
 
+
+def parse_cmd_line_args():
+    parser = argparse.ArgumentParser(
+        prog='wbfbd',
+        description="Functional Bus Description Language compiler back-end for Wishbone written in Python.",
+    )
+
+    parser.add_argument(
+        '-v', '--version', help="Display version.", action="version", version=VERSION
+    )
+
+    parser.add_argument('main', help="Path to the main file.")
+
+    parser.add_argument('-d', help="Log debug messages.", action='store_true')
+
+    return parser.parse_args()
+
+
+def main():
+    cmd_line_args = parse_cmd_line_args()
+
 if __name__ == "__main__":
-    print("Hello from WbFBD")
+    main()
