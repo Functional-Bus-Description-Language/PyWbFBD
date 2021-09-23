3077a591729b7970f357a3b01e6be66286f00e08
Michał Kruszewski <mkru@protonmail.com>
2021-09-23 18:45:54 +0200

    Add command line arguments parsing.

--- main.py
+++ main.py
@@ -1,29 +1,16 @@
 #!/bin/python3
-
-import argparse
-
 VERSION = "0.0.0"
 
+import fbdl
 
-def parse_cmd_line_args():
-    parser = argparse.ArgumentParser(
-        prog='wbfbd',
-        description="Functional Bus Description Language compiler back-end for Wishbone written in Python.",
-    )
-
-    parser.add_argument(
-        '-v', '--version', help="Display version.", action="version", version=VERSION
-    )
-
-    parser.add_argument('main', help="Path to the main file.")
-
-    parser.add_argument('-d', help="Log debug messages.", action='store_true')
-
-    return parser.parse_args()
+from wbfbd import args
 
 
 def main():
-    cmd_line_args = parse_cmd_line_args()
+    cmd_line_args = args.parse(VERSION)
+
+    bus = fbdl.compile(cmd_line_args['main'])
+
 
 if __name__ == "__main__":
     main()
---
+++ wbfbd/args.py
@@ -0,0 +1,144 @@
+"""
+Custom module for arguments parsing.
+argparse is not handy in this particular case.
+"""
+import sys
+from pprint import pformat
+
+
+valid_targets = {
+    'python': {
+        'Flags': {'--help': "Display help.", '--no-asserts': "Do not include asserts."},
+        'Options': {'--path': "Path for output files."},
+    },
+    'vhdl': {
+        'Flags': {'--help': "Display help."},
+        'Options': {'--path': "Path for output files."},
+    },
+}
+
+
+HELP = """Functional Bus Description Language compiler back-end for Wishbone written in Python.
+Version {version}.
+
+Supported targets:"""
+for i, target in enumerate(valid_targets):
+    if i > 0:
+        HELP += ', '
+    else:
+        HELP += ' '
+    HELP += target
+HELP += """.\nTo check valid flags and options for a given target type: 'wbfbd {{target}} --help'.
+
+Usage:
+  wbfbd [{{target}} [target flag or option] ...] ... path/to/fbd/file/with/main/bus/
+
+  At least one target must be specified. The last argument is always a path
+  to the fbd file containing a definition for the main bus, unless it is
+  '-h', '--help', '-v' or '--version.'
+
+Flags:
+  -h, --help     Display help.
+  -v, --version  Display version."""
+
+
+def print_target_help(target):
+    if not valid_targets[target]['Flags'] and not valid_targets[target]['Options']:
+        print(f"Target '{target}' does not have any flags or options.")
+        exit(0)
+
+    print(f"wbfbd help for '{target}' target.\n")
+    if valid_targets[target]['Flags']:
+        print("Flags:")
+        for flag, desc in valid_targets[target]['Flags'].items():
+            print(f"  {flag: <{8}} {desc}")
+
+    if valid_targets[target]['Options']:
+        print("\nOptions:")
+        for opt, desc in valid_targets[target]['Options'].items():
+            print(f"  {opt: <{8}} {desc}")
+
+    exit(0)
+
+
+def parse(version):
+    cmd_line_args = {}
+    current_target = None
+    current_option = None
+    expect_argument = False
+
+    in_global_args = True
+
+    if len(sys.argv) == 1:
+        raise Exception("Missing target and main file.")
+
+    if len(sys.argv) == 2:
+        arg = sys.argv[1]
+        if arg in ['-h', '--help']:
+            print(HELP.format(version=version))
+            exit(0)
+        elif arg in ['-v', '--version']:
+            print(version)
+            exit(0)
+        else:
+            if arg in valid_targets:
+                raise Exception("No main file has been specified.")
+            else:
+                raise Exception(f"'{arg}' is not a valid target.\n")
+
+    for i, arg in enumerate(sys.argv[1:]):
+        if i == len(sys.argv) - 2:
+            if not arg.startswith('-'):
+                break
+
+        if in_global_args:
+            if arg in ['-h', '--help']:
+                print(HELP.format(version=version))
+                exit(0)
+            elif arg in ['-v', '--version']:
+                print(version)
+                exit(0)
+            elif arg[0] != '-':
+                in_global_args = False
+                if arg not in valid_targets:
+                    raise Exception(f"'{arg}' is not a valid target.\n")
+                current_target = arg
+                cmd_line_args[arg] = {}
+                continue
+            else:
+                raise Exception(f"Invalid option {arg}.")
+
+        if expect_argument:
+            cmd_line_args[current_target][current_option] = arg
+            expect_argument = False
+        elif arg in valid_targets:
+            current_target = arg
+            continue
+        elif (
+            arg not in valid_targets[current_target]['Options']
+            and arg not in valid_targets[current_target]['Flags']
+            and expect_argument == False
+        ):
+            raise Exception(
+                f"'{arg}' is not a valid flag or option for '{current_target}' target.\n"
+                + f"Run 'wbfbd {current_target} --help' to see valid flags and options."
+            )
+        elif arg in ['--help']:
+            print_target_help(current_target)
+        elif arg in valid_targets[current_target]['Flags']:
+            # Use None as the value as any further checks will simply check if flag is in the dictionary.
+            cmd_line_args[current_target][arg] = None
+        elif arg in valid_targets[current_target]['Options']:
+            # Use None as the value as any further checks will simply check if flag is in the dictionary.
+            cmd_line_args[current_target][arg] = None
+            current_option = arg
+            expect_argument = True
+
+    if expect_argument:
+        raise Exception(
+            f"Missing argument for '{current_option}' option, target '{current_target}'."
+        )
+
+    cmd_line_args['main'] = sys.argv[-1]
+
+    return cmd_line_args
